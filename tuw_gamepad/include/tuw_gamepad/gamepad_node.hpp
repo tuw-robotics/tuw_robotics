eb92c4ff61a2d0190ec25c65505e1e617a7aa2ba
Markus Bader <markus.bader@tuwien.ac.at>
2025-06-03 08:00:34 +0000

    minor

--- tuw_gamepad/include/tuw_gamepad/gamepad_node.hpp
+++ tuw_gamepad/include/tuw_gamepad/gamepad_node.hpp
@@ -3,6 +3,7 @@
 
 #include <memory>
 #include <thread>
+#include <string>
 #include <tuw/node.hpp>
 #include <geometry_msgs/msg/twist_stamped.hpp>
 
@@ -35,19 +36,19 @@ namespace tuw_gamepad
 
     std::shared_ptr<tuw::Joystick> joystick_;
 
-    double rate_;               /// dynamic parameter: Publishing rate in [hz]
-    std::string frame_id_;      /// static parameter: Frame used in TwistStamped messages
-    std::string device_;        /// static parameter: Joystick device default "/dev/input/js0"
-    bool use_stamped_velocity_; /// static parameter: On true it uses TwistStamped messages, otherwise Twist!
-    int button_deadman_;        /// static parameter: deadmean button id
-    double scale_linear_;            /// static parameter: scale in linear  [m/s]
-    double scale_angular_;            /// static parameter: scale in rotation  [rad/s]
-    int axis_linear_x_;               /// static parameter: axis id of x velocity
-    int axis_angular_x_;               /// static parameter: axis id of x rotation
-    int axis_linear_y_;               /// static parameter: axis id of y velocity
-    int axis_angular_y_;               /// static parameter: axis id of y rotation
-    int axis_linear_z_;               /// static parameter: axis id of z velocity
-    int axis_angular_z_;               /// static parameter: axis id of z rotation
+    double rate_;              /// dynamic parameter: Publishing rate in [hz]
+    std::string frame_id_;     /// static parameter: Frame used in TwistStamped messages
+    std::string device;        /// static parameter: Joystick device default "/dev/input/js0"
+    bool use_stamped_velocity; /// static parameter: On true it uses TwistStamped messages, otherwise Twist!
+    int button_deadman_;       /// static parameter: deadmean button id
+    double scale_linear_;      /// static parameter: scale in linear  [m/s]
+    double scale_angular_;     /// static parameter: scale in rotation  [rad/s]
+    int axis_linear_x_;        /// static parameter: axis id of x velocity
+    int axis_angular_x_;       /// static parameter: axis id of x rotation
+    int axis_linear_y_;        /// static parameter: axis id of y velocity
+    int axis_angular_y_;       /// static parameter: axis id of y rotation
+    int axis_linear_z_;        /// static parameter: axis id of z velocity
+    int axis_angular_z_;       /// static parameter: axis id of z rotation
 
     void declare_parameters();      // declare parameters
     void read_static_parameters();  // ready the static parameters
