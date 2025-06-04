d2e38f77d4a957630f437f6fa3e4431352f7686e
MoritzTaferner <moritztaf@gmail.com>
2025-06-04 09:47:45 +0000

    fix typo

--- tuw_gamepad/include/tuw_gamepad/gamepad_node.hpp
+++ tuw_gamepad/include/tuw_gamepad/gamepad_node.hpp
@@ -38,8 +38,8 @@ namespace tuw_gamepad
 
     double rate_;              /// dynamic parameter: Publishing rate in [hz]
     std::string frame_id_;     /// static parameter: Frame used in TwistStamped messages
-    std::string device;        /// static parameter: Joystick device default "/dev/input/js0"
-    bool use_stamped_velocity; /// static parameter: On true it uses TwistStamped messages, otherwise Twist!
+    std::string device_;        /// static parameter: Joystick device default "/dev/input/js0"
+    bool use_stamped_velocity_;  /// static parameter: On true it uses TwistStamped messages, otherwise Twist!
     int button_deadman_;       /// static parameter: deadmean button id
     double scale_linear_;      /// static parameter: scale in linear  [m/s]
     double scale_angular_;     /// static parameter: scale in rotation  [rad/s]
